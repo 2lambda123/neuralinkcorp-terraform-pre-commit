9e070bcd64362044076ef592de82a36fe76f14aa
n7k-dobri <114534264+n7k-dobri@users.noreply.github.com>
2022-09-27 16:33:05 -0700

    Pick up .tflint.hcl from closest parent dir

--- hooks/tflint.sh
+++ hooks/tflint.sh
@@ -11,6 +11,20 @@ export PATH=$PATH:/usr/local/bin
 # During CI, the image should already have the desired plugins cached, so we're not blasting GitHub
 # too many times.
 
-for file in "$@"; do
-  tflint $file
+for file in "${@:2}"; do
+  CMD="tflint"
+
+  # Find first tflint config in folder hierarchy
+  CONFIG_PATH=$(readlink -e ${file%/*})
+  while [[ "$CONFIG_PATH" != "" && ! -e "$CONFIG_PATH/.tflint.hcl" ]]; do
+    CONFIG_PATH="${CONFIG_PATH%/*}"
+  done
+
+  # If .tflint is found, pass that to command
+  if [ "$CONFIG_PATH" != "" ]; then
+    CONFIG_FILE="$CONFIG_PATH/.tflint.hcl"
+    CMD="tflint -c $CONFIG_FILE"
+  fi
+
+  TFLINT_LOG=debug $CMD $file
 done
